3022ab33448048fdf42642b65f50cfa0b68019cf
AlbertoGongora <gongorabada@gmail.com>
2024-04-02 17:06:02 +0200

    agregar imagen previsualizacion

--- src/components/header/MenuLinks.jsx
+++ src/components/header/MenuLinks.jsx
@@ -22,7 +22,7 @@ export const MenuLinks = () => {
                 </a>
             </li>
             <li className="menuwhatsapp">
-            <a href="https://api.whatsapp.com/send?phone=34637492305&text=Hola%2C%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20acerca%20de%20sus%20proyectos.
+            <a href="https://api.whatsapp.com/send?phone=637492305&text=Hola%2C%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20acerca%20de%20sus%20proyectos.
 " target="_blank" rel="noreferrer">
                 <h3>Whatsapp</h3>
                 <i className="fab fa-whatsapp"></i>
