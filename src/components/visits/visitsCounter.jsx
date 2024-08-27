37cc86b6eed93f6319c7dead7c17b911f5c89635
AlbertoGongora <gongorabada@gmail.com>
2024-08-28 00:14:56 +0200

    Inserta datos en entorno para las credenciales

--- src/components/visits/visitsCounter.jsx
+++ src/components/visits/visitsCounter.jsx
@@ -7,18 +7,32 @@ export const VisitCounter = () => {
 
   useEffect(() => {
     const updateVisitCount = async () => {
-      const docRef = doc(db, 'visitCounter', 'pageVisits');
-      const docSnap = await getDoc(docRef);
+      // Verifica si ya se ha contado la visita en esta sesión
+      if (!sessionStorage.getItem('visited')) {
+        const docRef = doc(db, 'visitCounter', 'pageVisits');
+        const docSnap = await getDoc(docRef);
 
-      if (docSnap.exists()) {
-        const currentVisits = docSnap.data().count;
-        console.log("Current visits from Firestore:", currentVisits); // Depuración
-        setVisits(currentVisits + 1);
-        await setDoc(docRef, { count: currentVisits + 1 });
+        if (docSnap.exists()) {
+          const currentVisits = docSnap.data().count;
+          console.log("Current visits from Firestore:", currentVisits); // Depuración
+          setVisits(currentVisits + 1);
+          await setDoc(docRef, { count: currentVisits + 1 });
+        } else {
+          console.log("Document does not exist, initializing to 1"); // Depuración
+          setVisits(1);
+          await setDoc(docRef, { count: 1 });
+        }
+
+        // Marca la visita como contada en la sesión
+        sessionStorage.setItem('visited', 'true');
       } else {
-        console.log("Document does not exist, initializing to 1"); // Depuración
-        setVisits(1);
-        await setDoc(docRef, { count: 1 });
+        // Si ya se contó la visita en esta sesión, solo muestra el número actual
+        const docRef = doc(db, 'visitCounter', 'pageVisits');
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const currentVisits = docSnap.data().count;
+          setVisits(currentVisits);
+        }
       }
     };
 
--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Configuración de Firebase para tu aplicación web
 const firebaseConfig = {
-  apiKey: "AIzaSyC6uW5DHFVhzxe617AREqQf5OGUV711Ahc",
-  authDomain: "portafolio-alberto-gongora.firebaseapp.com",
-  projectId: "portafolio-alberto-gongora",
-  storageBucket: "portafolio-alberto-gongora.appspot.com",
-  messagingSenderId: "818117801770",
-  appId: "1:818117801770:web:0d092c6d05ab682d3bd053",
-  measurementId: "G-63FW1VFKEK"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Inicializa Firebase con la configuración proporcionada
@@ -18,5 +17,5 @@ const app = initializeApp(firebaseConfig);
 // Inicializa Firestore para la base de datos
 const db = getFirestore(app);
 
-// Exporta db y analytics para que puedas usarlos en otros componentes
+// Exporta db para que puedas usarlo en otros componentes
 export { db };
